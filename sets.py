d209ea17f0b21e2729bccfd3788c432c5c6e0981
thezaeemaanwar <thezaeemaanwar@gmail.com>
2022-06-01 17:07:59 +0500

    👌🏼IMPROVE: Prime Numbers using lambda

--- sets.py
+++ sets.py
@@ -1,5 +1,10 @@
-mySet = {2, 4, 31, 5, 6, 7, 8, 10}
+mySet = {2, 4, 31, 5, 6, 7, 8, 10, 13, 19, 51}
 
 print("\nGet Even Numbers from set: ")
 evenNumbers = set(filter(lambda el: el % 2 == 0, mySet))
 print("E = ", evenNumbers)
+
+
+print("\nPrime Numbers from set: ")
+primeNumbers = set(filter(lambda n: n%2 and n%3 and n%5 and n%7, mySet))
+print("P = ",primeNumbers)
