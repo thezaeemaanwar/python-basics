79e6b2da0392283f06e5ba60a64eaf132f2a66ef
thezaeemaanwar <thezaeemaanwar@gmail.com>
2022-06-01 16:09:47 +0500

    👌🏼 IMPROVE: Pep8 guidelines

--- sets.py
+++ sets.py
@@ -1,5 +1,5 @@
-mySet={2,4,31,5,6,7,8,10}
+mySet = {2, 4, 31, 5, 6, 7, 8, 10}
 
 print("\nGet Even Numbers from set: ")
-evenNumbers = set(filter(lambda el:el%2==0, mySet))
+evenNumbers = set(filter(lambda el: el % 2 == 0, mySet))
 print("E = ", evenNumbers)
